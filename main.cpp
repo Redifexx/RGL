89b928764adadb11fa4dc9f4765d4fcd95a4f4c1
Gio <gohan1703@gmail.com>
2023-11-20 19:42:51 -0500

    TRIFORCE!

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ const char* tempFragShader = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 "}\0";
 
 
@@ -29,8 +29,8 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	int winWidth = 1600;
-	int winHeight = 900;
+	int winWidth = 900;
+	int winHeight = 675;
 	//Creating Window Object
 	GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "RGL", NULL, NULL);
 	if (window == NULL)
@@ -55,8 +55,24 @@ int main()
 	//VBOs - Memory on the GPU allocated towards large batches of vertex data
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
+
+	//VAOs - Store attribute data for VBOs, makes updating VBOs far simpler
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//EBOs - prevents vertex overlap using indices
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	//Bind EBOs
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectIndices), rectIndices, GL_STATIC_DRAW);
+
+
+	//Bind VBOs
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triforce), triforce, GL_STATIC_DRAW);
 
 	//Vertex Shader Stuff
 	unsigned int vertexShader;
@@ -70,18 +86,40 @@ int main()
 	glShaderSource(fragmentShader, 1, &tempFragShader, NULL);
 	glCompileShader(fragmentShader);
 
+	//Shader Program, baby
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//How to interpret the vertex data per vertex attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	//Render Loop
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-		glClearColor(0.0f, 0.13f, 0.17f, 1.0f); //Bckground Color
+		//glClearColor(0.0f, 0.13f, 0.17f, 1.0f); //Bckground Color
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		//use it
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//draw it
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glBindVertexArray(0);
+		glDrawArrays(GL_TRIANGLES, 0, 9);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	//We dont need these anymore, gpu has them
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	glfwTerminate();
 
--- primitives.h
+++ primitives.h
@@ -4,3 +4,26 @@ float triangle[] = {
      0.5f, -0.5f, 0.0f,
      0.0f,  0.5f, 0.0f
 };
+
+float triforce[] = {
+    -0.25f, 0.0f, 0.0f,
+     0.25f, 0.0f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+     -0.5f, -0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+     -0.25f, 0.0f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.25f, 0.0f, 0.0f
+};
+
+float rectangle[] = {
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int rectIndices[] = {  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+};
