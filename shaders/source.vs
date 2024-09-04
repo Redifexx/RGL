21076f61675ee7e8249fbdfec0820259f0a9b0bc
Gio <44237862+Redifexx@users.noreply.github.com>
2024-09-04 10:49:43 -0400

    shaders

--- shaders/source.vs
+++ shaders/source.vs
@@ -6,9 +6,13 @@ layout (location = 2) in vec2 aTexCoord; // the texture variable has attribute p
 out vec3 ourColor; // output a color to the fragment shader
 out vec2 TexCoord;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     ourColor = aColor; // set ourColor to the input color we got from the vertex data
     TexCoord = aTexCoord;
 }    
--- src/main.cpp
+++ src/main.cpp
@@ -130,6 +130,31 @@ int main()
     }
     stbi_image_free(data); // cleans memory
 
+    //Matrix Transformations
+    // create transformations
+    glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+    glm::mat4 view          = glm::mat4(1.0f);
+    glm::mat4 projection    = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    // retrieve the matrix uniform locations
+    unsigned int modelLoc = glGetUniformLocation(curShader.ID, "model");
+    unsigned int viewLoc  = glGetUniformLocation(curShader.ID, "view");
+    // pass them to the shaders (3 different ways)
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+    //curShader.setMat4("projection", projection);
+
+
+    int modelLoc = glGetUniformLocation(curShader.ID, "model");
+    int modelLoc = glGetUniformLocation(curShader.ID, "model");
+
+
+
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
 
     //MAIN RENDER LOOP
     while(!glfwWindowShouldClose(window))
