d1934df4b9b709f5d61dde383a0c4e20e1eec284
Gio <gohan1703@gmail.com>
2024-09-19 16:48:55 -0400

    texture coords update
    texcoords update

--- src/block.cpp
+++ src/block.cpp
@@ -7,7 +7,7 @@ Block::Block(glm::ivec3 pos_, BLOCK_TYPE type_)
 }
 
 // Big ugly switch case
-glm::vec2 Block::GetBlockTexCoords()
+std::vector<glm::vec2> Block::GetBlockTexCoords()
 {
     std::vector<glm::vec2> texTop;
     std::vector<glm::vec2> texBottom;
@@ -15,48 +15,163 @@ glm::vec2 Block::GetBlockTexCoords()
     std::vector<glm::vec2> texRight;
     std::vector<glm::vec2> texFront;
     std::vector<glm::vec2> texBack;
+    std::vector<glm::vec2> texCoords;
 
     switch(this->type)
     {
         case STONE:
             //Top
-            texTop.push_back(glm::vec2(0.0f, 0.0f));
-            texTop.push_back(glm::vec2(0.1f, 0.0f));
-            texTop.push_back(glm::vec2(0.1f, 1.0f));
-            texTop.push_back(glm::vec2(0.0f, 1.0f));
+            texCoords.push_back(glm::vec2(0.0f, 0.9375f));
+            texCoords.push_back(glm::vec2(0.0625f, 0.9375f));
+            texCoords.push_back(glm::vec2(0.0625f, 1.0f));
+            texCoords.push_back(glm::vec2(0.0f, 1.0f));
 
             // Bottom
-            texBottom.push_back(glm::vec2(0.0f, 0.0f));
-            texBottom.push_back(glm::vec2(0.1f, 0.0f));
-            texBottom.push_back(glm::vec2(0.1f, 1.0f));
+            texBottom.push_back(glm::vec2(0.0f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.0625f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.0625f, 1.0f));
             texBottom.push_back(glm::vec2(0.0f, 1.0f));
 
             // Left
-            texLeft.push_back(glm::vec2(0.0f, 0.0f));
-            texLeft.push_back(glm::vec2(0.1f, 0.0f));
-            texLeft.push_back(glm::vec2(0.1f, 1.0f));
+            texLeft.push_back(glm::vec2(0.0f, 0.9375f));
+            texLeft.push_back(glm::vec2(0.0625f, 0.9375f));
+            texLeft.push_back(glm::vec2(0.0625f, 1.0f));
             texLeft.push_back(glm::vec2(0.0f, 1.0f));
 
             // Right
-            texRight.push_back(glm::vec2(0.0f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 1.0f));
+            texRight.push_back(glm::vec2(0.0f, 0.9375f));
+            texRight.push_back(glm::vec2(0.0625f, 0.9375f));
+            texRight.push_back(glm::vec2(0.0625f, 1.0f));
             texRight.push_back(glm::vec2(0.0f, 1.0f));
 
             // Front
-            texFront.push_back(glm::vec2(0.0f, 0.0f));
-            texFront.push_back(glm::vec2(0.1f, 0.0f));
-            texFront.push_back(glm::vec2(0.1f, 1.0f));
+            texFront.push_back(glm::vec2(0.0f, 0.9375f));
+            texFront.push_back(glm::vec2(0.0625f, 0.9375f));
+            texFront.push_back(glm::vec2(0.0625f, 1.0f));
             texFront.push_back(glm::vec2(0.0f, 1.0f));
 
             // Back
-            texRight.push_back(glm::vec2(0.0f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 1.0f));
+            texRight.push_back(glm::vec2(0.0f, 0.9375f));
+            texRight.push_back(glm::vec2(0.0625f, 0.9375f));
+            texRight.push_back(glm::vec2(0.0625f, 1.0f));
             texRight.push_back(glm::vec2(0.0f, 1.0f));
             break;
 
         case GOLD:
+            //Top
+            texTop.push_back(glm::vec2(0.0625f, 0.9375f));
+            texTop.push_back(glm::vec2(0.125f, 0.9375f));
+            texTop.push_back(glm::vec2(0.125f, 1.0f));
+            texTop.push_back(glm::vec2(0.0625f, 1.0f));
+
+            // Bottom
+            texBottom.push_back(glm::vec2(0.0625f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.125f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.125f, 1.0f));
+            texBottom.push_back(glm::vec2(0.0625f, 1.0f));
+
+            // Left
+            texLeft.push_back(glm::vec2(0.125f, 0.9375f)); // 2 AND 3
+            texLeft.push_back(glm::vec2(0.1875f, 0.9375f));
+            texLeft.push_back(glm::vec2(0.1875f, 1.0f));
+            texLeft.push_back(glm::vec2(0.125f, 1.0f));
+
+            // Right
+            texRight.push_back(glm::vec2(0.125f, 0.9375f)); 
+            texRight.push_back(glm::vec2(0.1875f, 0.9375f));
+            texRight.push_back(glm::vec2(0.1875f, 1.0f));
+            texRight.push_back(glm::vec2(0.125f, 1.0f));
+
+            // Front
+            texFront.push_back(glm::vec2(0.125f, 0.9375f)); 
+            texFront.push_back(glm::vec2(0.1875f, 0.9375f));
+            texFront.push_back(glm::vec2(0.1875f, 1.0f));
+            texFront.push_back(glm::vec2(0.125f, 1.0f));
+
+            // Back
+            texRight.push_back(glm::vec2(0.125f, 0.9375f)); 
+            texRight.push_back(glm::vec2(0.1875f, 0.9375f));
+            texRight.push_back(glm::vec2(0.1875f, 1.0f));
+            texRight.push_back(glm::vec2(0.125f, 1.0f));
+            break;
+            
+        case GRASS:
+            //Top
+            texTop.push_back(glm::vec2(0.25f, 0.9375f));
+            texTop.push_back(glm::vec2(0.3125f, 0.9375f));
+            texTop.push_back(glm::vec2(0.3125f, 1.0f));
+            texTop.push_back(glm::vec2(0.25f, 1.0f));
+
+            // Bottom
+            texBottom.push_back(glm::vec2(0.625f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.6875f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.6875f, 1.0f));
+            texBottom.push_back(glm::vec2(0.625f, 1.0f));
+
+            // Left
+            texLeft.push_back(glm::vec2(0.1875f, 0.9375f));     // 2 AND 3
+            texLeft.push_back(glm::vec2(0.25f, 0.9375f));
+            texLeft.push_back(glm::vec2(0.25f, 1.0f));
+            texLeft.push_back(glm::vec2(0.1875f, 1.0f));
+
+            // Right
+            texRight.push_back(glm::vec2(0.1875f, 0.9375f));
+            texRight.push_back(glm::vec2(0.25f, 0.9375f));
+            texRight.push_back(glm::vec2(0.25f, 1.0f));
+            texRight.push_back(glm::vec2(0.1875f, 1.0f));
+
+            // Front
+            texFront.push_back(glm::vec2(0.1875f, 0.9375f));
+            texFront.push_back(glm::vec2(0.25f, 0.9375f));
+            texFront.push_back(glm::vec2(0.25f, 1.0f));
+            texFront.push_back(glm::vec2(0.1875f, 1.0f));
+
+            // Back
+            texRight.push_back(glm::vec2(0.1875f, 0.9375f));
+            texRight.push_back(glm::vec2(0.25f, 0.9375f));
+            texRight.push_back(glm::vec2(0.25f, 1.0f));
+            texRight.push_back(glm::vec2(0.1875f, 1.0f));
+            break;
+
+        case GRAVEL:
+            //Top
+            texTop.push_back(glm::vec2(0.3125f, 0.9375f));
+            texTop.push_back(glm::vec2(0.375f, 0.9375f));
+            texTop.push_back(glm::vec2(0.375f, 1.0f));
+            texTop.push_back(glm::vec2(0.3125f, 1.0f));
+
+            // Bottom
+            texBottom.push_back(glm::vec2(0.3125f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.375f, 0.9375f));
+            texBottom.push_back(glm::vec2(0.375f, 1.0f));
+            texBottom.push_back(glm::vec2(0.3125f, 1.0f));
+
+            // Left
+            texLeft.push_back(glm::vec2(0.3125f, 0.9375f));       // 2 AND 3
+            texLeft.push_back(glm::vec2(0.375f, 0.9375f));
+            texLeft.push_back(glm::vec2(0.375f, 1.0f));
+            texLeft.push_back(glm::vec2(0.3125f, 1.0f));
+
+            // Right
+            texRight.push_back(glm::vec2(0.3125f, 0.9375f));
+            texRight.push_back(glm::vec2(0.375f, 0.9375f));
+            texRight.push_back(glm::vec2(0.375f, 1.0f));
+            texRight.push_back(glm::vec2(0.3125f, 1.0f));
+
+            // Front
+            texFront.push_back(glm::vec2(0.3125f, 0.9375f));
+            texFront.push_back(glm::vec2(0.375f, 0.9375f));
+            texFront.push_back(glm::vec2(0.375f, 1.0f));
+            texFront.push_back(glm::vec2(0.3125f, 1.0f));
+
+            // Back
+            texRight.push_back(glm::vec2(0.3125f, 0.9375f));
+            texRight.push_back(glm::vec2(0.375f, 0.9375f));
+            texRight.push_back(glm::vec2(0.375f, 1.0f));
+            texRight.push_back(glm::vec2(0.3125f, 1.0f));
+            break;
+
+        case IRON: //TBD
             //Top
             texTop.push_back(glm::vec2(0.1f, 0.0f));
             texTop.push_back(glm::vec2(0.2f, 0.0f));
@@ -70,28 +185,104 @@ glm::vec2 Block::GetBlockTexCoords()
             texBottom.push_back(glm::vec2(0.1f, 1.0f));
 
             // Left
-            texLeft.push_back(glm::vec2(0.0f, 0.0f)); // 2 AND 3
-            texLeft.push_back(glm::vec2(0.1f, 0.0f));
-            texLeft.push_back(glm::vec2(0.1f, 1.0f));
-            texLeft.push_back(glm::vec2(0.0f, 1.0f));
+            texLeft.push_back(glm::vec2(0.2f, 0.0f)); // 2 AND 3
+            texLeft.push_back(glm::vec2(0.3f, 0.0f));
+            texLeft.push_back(glm::vec2(0.3f, 1.0f));
+            texLeft.push_back(glm::vec2(0.2f, 1.0f));
 
             // Right
-            texRight.push_back(glm::vec2(0.0f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 1.0f));
-            texRight.push_back(glm::vec2(0.0f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
 
             // Front
-            texFront.push_back(glm::vec2(0.0f, 0.0f));
-            texFront.push_back(glm::vec2(0.1f, 0.0f));
-            texFront.push_back(glm::vec2(0.1f, 1.0f));
-            texFront.push_back(glm::vec2(0.0f, 1.0f));
+            texFront.push_back(glm::vec2(0.2f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 1.0f));
+            texFront.push_back(glm::vec2(0.2f, 1.0f));
 
             // Back
-            texRight.push_back(glm::vec2(0.0f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 0.0f));
-            texRight.push_back(glm::vec2(0.1f, 1.0f));
-            texRight.push_back(glm::vec2(0.0f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
+            break;
+
+        case SPONGE: //TBD
+            //Top
+            texTop.push_back(glm::vec2(0.1f, 0.0f));
+            texTop.push_back(glm::vec2(0.2f, 0.0f));
+            texTop.push_back(glm::vec2(0.2f, 1.0f));
+            texTop.push_back(glm::vec2(0.1f, 1.0f));
+
+            // Bottom
+            texBottom.push_back(glm::vec2(0.1f, 0.0f));
+            texBottom.push_back(glm::vec2(0.2f, 0.0f));
+            texBottom.push_back(glm::vec2(0.2f, 1.0f));
+            texBottom.push_back(glm::vec2(0.1f, 1.0f));
+
+            // Left
+            texLeft.push_back(glm::vec2(0.2f, 0.0f)); // 2 AND 3
+            texLeft.push_back(glm::vec2(0.3f, 0.0f));
+            texLeft.push_back(glm::vec2(0.3f, 1.0f));
+            texLeft.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Right
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Front
+            texFront.push_back(glm::vec2(0.2f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 1.0f));
+            texFront.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Back
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
+            break;
+            
+        default:
+            //Top
+            texTop.push_back(glm::vec2(0.1f, 0.0f));
+            texTop.push_back(glm::vec2(0.2f, 0.0f));
+            texTop.push_back(glm::vec2(0.2f, 1.0f));
+            texTop.push_back(glm::vec2(0.1f, 1.0f));
+
+            // Bottom
+            texBottom.push_back(glm::vec2(0.1f, 0.0f));
+            texBottom.push_back(glm::vec2(0.2f, 0.0f));
+            texBottom.push_back(glm::vec2(0.2f, 1.0f));
+            texBottom.push_back(glm::vec2(0.1f, 1.0f));
+
+            // Left
+            texLeft.push_back(glm::vec2(0.2f, 0.0f)); // 2 AND 3
+            texLeft.push_back(glm::vec2(0.3f, 0.0f));
+            texLeft.push_back(glm::vec2(0.3f, 1.0f));
+            texLeft.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Right
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Front
+            texFront.push_back(glm::vec2(0.2f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 0.0f));
+            texFront.push_back(glm::vec2(0.3f, 1.0f));
+            texFront.push_back(glm::vec2(0.2f, 1.0f));
+
+            // Back
+            texRight.push_back(glm::vec2(0.2f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 0.0f));
+            texRight.push_back(glm::vec2(0.3f, 1.0f));
+            texRight.push_back(glm::vec2(0.2f, 1.0f));
             break;
     }
 
--- src/block.h
+++ src/block.h
@@ -25,7 +25,7 @@ public:
     glm::ivec3 pos;
     BLOCK_TYPE type;
     Block(glm::ivec3 pos_ = glm::ivec3(0), BLOCK_TYPE type_ = AIR);
-    glm::vec2 GetBlockTexCoords();
+    std::vector<glm::vec2> GetBlockTexCoords();
 };
 
 #endif
