f2e3ac75e3ae523a15c9ed9d55c80818ea31fe8e
Gio <gohan1703@gmail.com>
2024-11-20 13:32:53 -0500

    working on render texture

---
+++ src/FrameBuffer.h
@@ -0,0 +1,22 @@
+//From https://uysalaltas.github.io/2022/01/09/OpenGL_Imgui.html
+
+#pragma once
+#include <iostream>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+class FrameBuffer
+{
+public:
+    FrameBuffer(float width, float height);
+    ~FrameBuffer();
+    unsigned int getFrameTexture();
+    void RescaleFrameBuffer(float width, float height);
+    void Bind() const;
+    void Unbind() const;
+
+private:
+    unsigned int fbo;
+    unsigned int texture;
+    unsigned int rbo;
+};
